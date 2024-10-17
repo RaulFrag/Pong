0380b464f00da3a483ba6524c87496181af4844b
Raul <raul.fragoso@epiagranollers.cat>
2024-10-17 17:38:10 +0200

    Algunas cosas agregadas al main

---
+++ Pong/Ball.h
@@ -0,0 +1,12 @@
+#pragma once
+class Ball
+{
+public:
+	Ball();
+	~Ball();
+
+	void init();
+	void update();
+	void render();
+};
+
--- Pong/main.cpp
+++ Pong/main.cpp
@@ -5,6 +5,21 @@
 using namespace std;
 
 int main(int argr, char* args[]) {
-	
+	SDL_Window* window = NULL;
+	SDL_Surface* screenSurface = NULL;
+
+	SDL_Init(SDL_INIT_VIDEO);
+	window = SDL_CreateWindow("Ex 5",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		854, 480,
+		SDL_WINDOW_SHOWN);
+
+	screenSurface = SDL_GetWindowSurface(window);
+
+	while (true)
+	{
+		
+	}
 	return 0;
 }
